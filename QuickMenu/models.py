d611ee3deaba603a072e0ad4cdedf7d3fcd642f1
Tyler Reher <trreher@gmail.com>
2021-02-14 14:31:19 -0600

    Correcting models

--- QuickMenu/models.py
+++ QuickMenu/models.py
@@ -6,7 +6,7 @@ from django.urls import reverse
 class Ingredient(models.Model):
     name = models.CharField(max_length=50,blank=False, null=False, default=' ')
     description = models.CharField(max_length=50, blank=True, null=True,default=' ')
-    purchaseAmount = model.DecimalField(max_digits=10, decimal_places=3, null=False, default=' ')
+    purchaseAmount = models.DecimalField(max_digits=10, decimal_places=3, null=False, default=' ')
     lastPurchasePrice = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=' ')
     lastPurchaseDate = models.DateTimeField(auto_now_add=True)
     measurementType = models.CharField(max_length=10, default='g')
